1d475d3d9b5207e1e84d385b6c257e79ccc65be4
Himanshu <himanshuraj48512@gmail.com>
2026-01-20 03:53:07 +0530

    Sidebar Completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useState } from "react";
+import Sidebar from "./components/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import ChatBox from "./components/ChatBox";
+import Credits from "./pages/Credits";
+import Community from "./pages/Community";
+import { assets } from "./assets/assets";
 
 const App = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <div>App</div>
-  )
-}
+    <>
+      {!isMenuOpen && (
+        <img
+          src={assets.menu_icon}
+          className="absolute top-3 left-3 w-8 h-8 cursor-pointer md:hidden not-dark:invert"
+          onClick={() => setIsMenuOpen(true)}
+        />
+      )}
+      <div className="dark:bg-gradient-to-b from-[#242124] to-[#000000] dark:text-white">
+        <div className="flex h-screen w-screen">
+          <Sidebar isMenuOpen={isMenuOpen} setIsMenuOpen={setIsMenuOpen} />
+          <Routes>
+            <Route path="/" element={<ChatBox />} />
+            <Route path="/credits" element={<Credits />} />
+            <Route path="/community" element={<Community />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
